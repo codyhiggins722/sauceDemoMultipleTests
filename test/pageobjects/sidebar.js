cb68ee674c8d02b0da0c4f054eb1f8d514c2dc11
codyhiggins <cody.higgins9046@stu.mtec.edu>
2026-04-07 13:26:36 -0600

    making sidebar.js prettier

--- test/pageobjects/sidebar.js
+++ test/pageobjects/sidebar.js
@@ -25,108 +25,108 @@ class HamburgerMenu extends Site{
         return $('[href="/request-demo"]')
     }
     async menuNavigation(){
-    await this.hamburgerBtn.click();
-    await this.allItems.waitForClickable({ timeout: 3000 });
-    const originalUrl = await browser.getUrl();
-    await this.allItems.click();
-        if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.cartButton.click();
-            await expect (CartPage.continueShopping).toBeExisting();
-            await this.hamburgerBtn.click();
+        await this.hamburgerBtn.click();
+        await this.allItems.waitForClickable({ timeout: 3000 });
+        const originalUrl = await browser.getUrl();
+        await this.allItems.click();
+            if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.cartButton.click();
+                await expect (CartPage.continueShopping).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+            else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.clickRandomItemImg();
+                await expect (itemDetail.backToProducts).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+        await this.resetState.click();
+        await expect (HomePage.itemInCart).not.toBeExisting();
+        const aboutUrl = await this.about.getAttribute('href');
+        await expect(aboutUrl).toContain('saucelabs.com');
+        await this.closeButton.click();
         }
-        else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.clickRandomItemImg();
-            await expect (itemDetail.backToProducts).toBeExisting();
-            await this.hamburgerBtn.click();
-        }
-    await this.resetState.click();
-    await expect (HomePage.itemInCart).not.toBeExisting();
-    const aboutUrl = await this.about.getAttribute('href');
-    await expect(aboutUrl).toContain('saucelabs.com');
-    await this.closeButton.click();
-    }
     async returntoInventory(){
         await this.hamburgerBtn.click();
         await this.allItems.click();
         await expect(HomePage.landingPage).toBeExisting();
     }
     async menuNavigationAbout(){
-    await this.hamburgerBtn.click();
-    await this.allItems.waitForClickable({ timeout: 3000 });
-    const originalUrl = await browser.getUrl();
-    await this.allItems.click();
-        if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.cartButton.click();
-            await expect (CartPage.continueShopping).toBeExisting();
-            await this.hamburgerBtn.click();
+        await this.hamburgerBtn.click();
+        await this.allItems.waitForClickable({ timeout: 3000 });
+        const originalUrl = await browser.getUrl();
+        await this.allItems.click();
+            if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.cartButton.click();
+                await expect (CartPage.continueShopping).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+            else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.clickRandomItemImg();
+                await expect (itemDetail.backToProducts).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+        await this.resetState.click();
+        await expect (HomePage.itemInCart).not.toBeExisting();
+        await browser.setTimeout({ 'pageload' : 5000});
+        try {
+            await this.about.click();
+        } catch (err) {
         }
-        else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.clickRandomItemImg();
-            await expect (itemDetail.backToProducts).toBeExisting();
-            await this.hamburgerBtn.click();
+        await browser.url('https://www.saucedemo.com/inventory.html');
+        await browser.setTimeout({ 'pageload': 30000});
+        const productTitle = await $('//span[contains(text(), "Products")]');
+        await productTitle.waitForDisplayed({ timeout: 10000 });
+        await this.hamburgerBtn.click();
+        await this.closeButton.click();
         }
-    await this.resetState.click();
-    await expect (HomePage.itemInCart).not.toBeExisting();
-    await browser.setTimeout({ 'pageload' : 5000});
-    try {
-        await this.about.click();
-    } catch (err) {
-    }
-    await browser.url('https://www.saucedemo.com/inventory.html');
-    await browser.setTimeout({ 'pageload': 30000});
-    const productTitle = await $('//span[contains(text(), "Products")]');
-    await productTitle.waitForDisplayed({ timeout: 10000 });
-    await this.hamburgerBtn.click();
-    await this.closeButton.click();
-    }
     async menuNavigationAboutReload(){
-    await this.hamburgerBtn.click();
-    await this.allItems.waitForClickable({ timeout: 3000 });
-    let originalUrl = await browser.getUrl();
-    await this.allItems.click();
-        if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.cartButton.click();
-            await expect (CartPage.continueShopping).toBeExisting();
-            await this.hamburgerBtn.click();
+        await this.hamburgerBtn.click();
+        await this.allItems.waitForClickable({ timeout: 3000 });
+        let originalUrl = await browser.getUrl();
+        await this.allItems.click();
+            if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.cartButton.click();
+                await expect (CartPage.continueShopping).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+            else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.clickRandomItemImg();
+                await expect (itemDetail.backToProducts).toBeExisting();
+                await this.hamburgerBtn.click();
+            }
+        await this.resetState.click();
+        await expect (HomePage.itemInCart).not.toBeExisting();
+        originalUrl = await browser.getUrl();
+        await browser.setTimeout({ 'pageload' : 5000});
+        try {
+            await this.about.click();
+        } catch (err) {
+            console.log('made it to about page, error due to timeout')
         }
-        else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.clickRandomItemImg();
-            await expect (itemDetail.backToProducts).toBeExisting();
-            await this.hamburgerBtn.click();
+        await browser.reloadSession();
+        await SauceLog.open();
+        await SauceLog.login('standard_user', 'secret_sauce');
+        const productTitle = await $('//span[contains(text(), "Products")]');
+        await productTitle.waitForDisplayed({ timeout: 10000 });
+            if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.cartButton.click();
+                await expect (CartPage.continueShopping).toBeExisting();
+            }
+            else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
+                await expect (HomePage.landingPage).toBeExisting();
+                await HomePage.clickRandomItemImg();
+                await expect (itemDetail.backToProducts).toBeExisting();
+            }    
+        await this.hamburgerBtn.click();
+        await this.closeButton.click();
         }
-    await this.resetState.click();
-    await expect (HomePage.itemInCart).not.toBeExisting();
-    originalUrl = await browser.getUrl();
-    await browser.setTimeout({ 'pageload' : 5000});
-    try {
-        await this.about.click();
-    } catch (err) {
-        console.log('made it to about page, error due to timeout')
-    }
-    await browser.reloadSession();
-    await SauceLog.open();
-    await SauceLog.login('standard_user', 'secret_sauce');
-    const productTitle = await $('//span[contains(text(), "Products")]');
-    await productTitle.waitForDisplayed({ timeout: 10000 });
-        if ((await this.currentURL !== originalUrl) && originalUrl.includes('cart.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.cartButton.click();
-            await expect (CartPage.continueShopping).toBeExisting();
-        }
-        else if ((await this.currentURL !== originalUrl) && originalUrl.includes('inventory-item.html')) {
-            await expect (HomePage.landingPage).toBeExisting();
-            await HomePage.clickRandomItemImg();
-            await expect (itemDetail.backToProducts).toBeExisting();
-        }    
-    await this.hamburgerBtn.click();
-    await this.closeButton.click();
-    }
 }
 
 export default new HamburgerMenu();
